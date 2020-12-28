6d597f3bd7ee51448a344b7db88ffda3d052ce71
yangdongdong333 <1391587437@qq.com>
2020-12-28 20:08:48 +0800

    继承Thread方式创建线程

---
+++ src/com/ydd/J10ConcurrentAndSynchronized/ConCurrentDownload.java
@@ -0,0 +1,6 @@
+package com.ydd.J10ConcurrentAndSynchronized;/**
+* @Author: yangdongdong
+* @CreateTime: 2020/12/28 19:11
+* @Description:
+*/public class ConCurrentDownload {
+}
---
+++ src/com/ydd/J10ConcurrentAndSynchronized/ExtendsThreadTest.java
@@ -0,0 +1,6 @@
+package com.ydd.J10ConcurrentAndSynchronized;/**
+* @Author: yangdongdong
+* @CreateTime: 2020/12/28 18:41
+* @Description:
+*/public class ExtendsThreadTest {
+}
