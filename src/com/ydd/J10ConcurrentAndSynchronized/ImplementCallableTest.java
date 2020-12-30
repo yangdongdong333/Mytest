be132282d37011772abf8ff911a4868914d8b9f4
yangdongdong333 <1391587437@qq.com>
2020-12-30 15:54:24 +0800

    实现多线程的继承Thread、实现Runnable、实现Callable

---
+++ src/com/ydd/J10ConcurrentAndSynchronized/ImplementCallableTest.java
@@ -0,0 +1,6 @@
+package com.ydd.J10ConcurrentAndSynchronized;/**
+* @Author: yangdongdong
+* @CreateTime: 2020/12/30 15:21
+* @Description:
+*/public class ImplementCallableTest {
+}
---
+++ src/com/ydd/J10ConcurrentAndSynchronized/Race.java
@@ -0,0 +1,6 @@
+package com.ydd.J10ConcurrentAndSynchronized;/**
+* @Author: yangdongdong
+* @CreateTime: 2020/12/30 9:09
+* @Description:
+*/public class Race {
+}
