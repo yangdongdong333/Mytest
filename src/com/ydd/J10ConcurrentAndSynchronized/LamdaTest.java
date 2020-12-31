128eabd8a80024cfc5ce3f0386714a54a6cb9f2b
yangdongdong333 <1391587437@qq.com>
2020-12-31 09:58:45 +0800

    静态代理与lambda表达式

---
+++ src/com/ydd/J10ConcurrentAndSynchronized/LamdaTest.java
@@ -0,0 +1,6 @@
+package com.ydd.J10ConcurrentAndSynchronized;/**
+* @Author: yangdongdong
+* @CreateTime: 2020/12/31 9:30
+* @Description:
+*/public class LamdaTest {
+}
---
+++ src/com/ydd/J10ConcurrentAndSynchronized/StaticProxy.java
@@ -0,0 +1,6 @@
+package com.ydd.J10ConcurrentAndSynchronized;/**
+* @Author: yangdongdong
+* @CreateTime: 2020/12/31 9:00
+* @Description:
+*/public class StaticProxy {
+}
